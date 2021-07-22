02fd827382656824f6b774a3de488b873b3b744a
Ryan Aunur <ryandevstudio@gmail.com>
2021-07-22 15:07:51 +0700

    update(component/results): use cors bridged and remove glitch

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -67,3 +67,35 @@ export const generateLink = (item, isShorten = false) => {
 
   return `https://cloudflare-ipfs.com/ipfs/${item.cid}`
 }
+
+/**
+ * Generate Short Link via s.id services
+ * @param {String} url 
+ * @returns {Promise<*[]>}
+ */
+export const generateShortLink = async (url) => {
+  try {
+    const response = await fetch(`https://cors.bridged.cc/https://home.s.id/api/public/link/shorten`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "x-csrf-token": "JfN0LkiBFPHfiBAVnHE61v5C3ti7hMB2uUb08VuI",
+        "x-requested-with": "XMLHttpRequest",
+        "x-xsrf-token": "JfN0LkiBFPHfiBAVnHE61v5C3ti7hMB2uUb08VuI",
+      },
+      body: `url=${encodeURIComponent(url)}`
+    });
+
+    if (!response.ok) {
+      throw new Error(`SOMETHING_WRONG`);
+    }
+
+    const data = await response.json();
+
+    if (data && data.short) {
+      return [ null, data ];
+    }
+  } catch (error) {
+    return [ new Error(`Ops! error while generate shorten link.`), null ];
+  }
+}
