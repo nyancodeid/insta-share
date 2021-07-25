eabdb38e880ac7ba590f6c38da3b3a13b2962b0e
Ryan Aunur <ryandevstudio@gmail.com>
2021-07-25 13:31:37 +0700

    new(component/prompt): PWA update prompt compnt

---
+++ src/components/ReloadPrompt.vue
@@ -0,0 +1,72 @@
+<template>
+  <div v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
+    <div class="message">
+      <span v-if="offlineReady"> App ready to work offline </span>
+      <span v-else>
+        New content available, click on reload button to update.
+      </span>
+    </div>
+    <button v-if="needRefresh" @click="updateServiceWorker()">Reload</button>
+    <button @click="close">Close</button>
+  </div>
+</template>
+
+<script>
+import { useRegisterSW } from "virtual:pwa-register/vue";
+
+const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW();
+
+export default {
+  name: "ReloadPrompt",
+  setup() {
+    const close = async () => {
+      offlineReady.value = false;
+      needRefresh.value = false;
+    };
+
+    return {
+      offlineReady,
+      needRefresh,
+      close
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.pwa-toast {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin: 16px;
+  padding: 12px;
+  border: 1px solid #8885;
+  border-radius: 4px;
+  z-index: 1;
+  text-align: left;
+  box-shadow: 3px 4px 5px 0 #8885;
+  background-color: rgb(66, 66, 66);
+  color: rgb(245, 245, 245);
+
+  .message {
+    margin-bottom: 8px;
+  }
+  button {
+    margin-right: 5px;
+    box-shadow: 0 1px 1px 0 rgb(0 0 0 / 14%), 0 2px 1px -1px rgb(0 0 0 / 12%),
+      0 1px 3px 0 rgb(0 0 0 / 20%);
+    background: rgb(245, 245, 245);
+    color: rgb(33, 33, 33);
+    border-radius: 2px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 14px;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    padding: 11px 16px;
+    display: inline-block;
+    width: 120px;
+  }
+}
+</style>
