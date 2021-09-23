32f49a2f10ee860e29063bc479e0eb3dcc1c9acd
nyancodeid <ryandevstudio@gmail.com>
2021-09-23 15:43:26 +0700

    update(view/about): add description

--- src/views/VAbout.vue
+++ src/views/VAbout.vue
@@ -5,7 +5,7 @@
         <h1>InstaShare<span class="emoji">⚡</span></h1>
         <p>an Instant File Sharing powered by IPFS Protocol. Build with <a href="https://v3.vuejs.org/" target="_blank" rel="noopener">Vue 3</a> and
           <a href="https://vitejs.dev/" target="_blank" rel="noopener">ViteJS</a>.</p>
-        <p>InstaShare (Instant Share) is a simple web-based application that simply upload your files to Interplanetary File System (IPFS) Network using <a href="https://infura.io/product/ipfs" target="_blank" rel="noopener">Infura API</a>.</p>
+        <p>InstaShare (Instant Share) is a simple web-based application that simply upload your files to Interplanetary File System (IPFS) Network using <a href="https://infura.io/product/ipfs" target="_blank" rel="noopener">Infura API</a>. File uploads cannot be deleted, hacked, edited, never saved to any server (decentralized) + are only accessable by using a hash / cid (content identifier).</p>
         <p>InstaShare build with Latest Web Technology that makes hopefully can access this app even in offline mode (cache-first). And the other cool thing is PWA (Progressive Web App) makes this app installable on Android devices, allow us to use this app to run native-like on an Android.</p>
 
         <p>Build with <i-ri-heart-2-fill class="icon-love" /> by <a href="https://github.com/nyancodeid" rel="noopener" class="author">NyanCodeID</a></p>
